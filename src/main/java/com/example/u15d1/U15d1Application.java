d33bf914604ba9bb01cd63e42d38368908d6a41a
AlessioLosi <alessiolosi93@icloud.com>
2024-10-21 15:31:40 +0200

    Initial commit

---
+++ src/main/java/com/example/u15d1/U15d1Application.java
@@ -0,0 +1,13 @@
+package com.example.u15d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U15d1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U15d1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/u15d1/U15d1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.u15d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U15d1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
